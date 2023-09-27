186f24eada9657a13e83787e9c598f9b9d45b1b5
Jason Liu <111265664+liug26@users.noreply.github.com>
2023-09-26 18:14:49 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 npm install
-python3 -m pip install --upgrade pip
-pip3 install pandas numpy argparse
-pip3 install plotly==5.15.0
+pip3 install pandas numpy argparse plotly
